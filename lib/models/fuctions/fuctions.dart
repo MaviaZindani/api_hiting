e8ebbacecae41f83bb830c02857f685aabcde458
MaviaZindani <maviazindaninfo@gmail.com>
2024-05-29 04:26:35 -0700

    second commit

--- lib/models/fuctions/fuctions.dart
+++ lib/models/fuctions/fuctions.dart
@@ -4,11 +4,10 @@ import 'package:http/http.dart' as http;
 
 //  getdata fuction
 
-
-
 Future<List<Dataofapi>> getApi() async {
   List<Dataofapi> postlist = [];
-  var url = Uri.parse("https://crudcrud.com/api/e26d72a0604f473593a774037a321519/unicorns");
+  var url = Uri.parse(
+      "https://crudcrud.com/api/e26d72a0604f473593a774037a321519/unicorns");
   var response = await http.get(url);
   var responseBody = jsonDecode(response.body);
 
@@ -23,14 +22,19 @@ Future postData() async {
   try {
     var url = Uri.parse(
         "https://crudcrud.com/api/e26d72a0604f473593a774037a321519/unicorns");
-    var response = await http.post(url, body: {
+   await http.post(
+      url, 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: jsonEncode({
       'age': 1.toString(),
       'colour': 'white',
       'sId': 'mavia',
       'name': 'irfan'
-    });
-    var responseBody = jsonEncode(response.body);
-    return responseBody;
+    }),
+    );
+    print(await http.read(Uri.https("https://crudcrud.com/api/e26d72a0604f473593a774037a321519/unicorns"),),);
   } catch (e) {
     print(e);
   }
